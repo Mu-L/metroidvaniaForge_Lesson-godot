dfa88d1241977d7476c1843cb8df56e71aa53c34
artludovicogamedev <artludovico@gmail.com>
2026-03-12 18:38:40 +0800

    persistent data not working

---
+++ general/Doors/door_interactible.gd
@@ -0,0 +1,42 @@
+@tool
+@icon("res://general/icons/door.svg")
+class_name DoorInteractible
+extends Node2D
+
+const DOOR_CRASH_AUDIO = preload("uid://csfqkrs6y48sj")
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var door_name : String = ""
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	
+	#connect to switch thru signal
+	for s in get_children():
+		if s is DoorSwitch :
+			s.activated.connect(on_switch_activated)
+			if s.is_door_opened :
+				on_switch_is_active()
+	pass
+
+func on_switch_activated() -> void : 
+	#audio playback
+	#play the animation open
+	animation_player.play("open")
+	pass
+	
+func on_switch_is_active() -> void :
+	#if opened, play the open animation
+	animation_player.play("opened")
+	pass
+	
+func _get_configuration_warnings() -> PackedStringArray:
+	if check_for_switches() == false :
+		return [ "Door Switch node missing."]
+	return []
+
+func check_for_switches() -> bool :
+	for c in get_children():
+		if c is DoorSwitch :
+			return true
+	return false
---
+++ general/Doors/door_switch.gd
@@ -0,0 +1,50 @@
+@icon("res://general/icons/door.svg")
+class_name DoorSwitch
+extends Node2D
+
+const DOOR_SWITCH_AUDIO = preload("uid://wahkxibc0c6m")
+
+signal activated
+@export var is_door_opened : bool = false
+@onready var sprite_2d: Sprite2D = %Sprite2D
+@onready var area_2d: Area2D = %Area2D
+
+func _ready() -> void:
+	if SaveManager.persistent_data.get_or_add( set_unique_name() ,"closed") == "open" :
+		set_open()
+	else :
+		area_2d.body_entered.connect(on_player_entered)
+		area_2d.body_exited.connect(on_player_exited)
+		pass
+	pass
+
+func on_player_entered(_n : Node2D) -> void :
+	Messages.input_hint_changed.emit("action")
+	Messages.player_interacted.connect(on_player_interacted)
+	pass
+
+func on_player_exited(_n : Node2D) -> void :
+	Messages.input_hint_changed.emit("")
+	Messages.player_interacted.disconnect(on_player_interacted)
+	pass
+	
+func on_player_interacted(_player :Player) -> void :
+	#audio
+	#persistent data
+	SaveManager.persistent_data[ set_unique_name() ] = "open"
+	activated.emit()
+	set_open()
+	print("Player interacted with switch")
+
+func set_open() -> void :
+	is_door_opened = true
+	sprite_2d.flip_h = true
+	sprite_2d.modulate = Color.GRAY
+	area_2d.queue_free()
+	pass
+
+func set_unique_name () -> String :
+	var uname :String = ResourceUID.path_to_uid(owner.scene_file_path) 
+	uname += "/" + get_parent().name + "/" + name
+	print(uname)
+	return uname
